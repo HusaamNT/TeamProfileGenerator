9e01436a258b87f68f875c8f82663aadbcfb8b7a
Husaam NT <husnaeem@gmail.com>
2022-12-06 13:55:23 +0000

    added write html file code

--- modules/engineer.js
+++ modules/engineer.js
@@ -8,7 +8,4 @@ class Engineer extends Profile{
     }
 
 }
-
-
-
 module.exports = Engineer
--- script.js
+++ script.js
@@ -4,6 +4,20 @@ const Engineer = require("./modules/engineer")
 const Intern = require("./modules/intern")
 const Manager = require ("./modules/manager")
 
+const htmlContent = (`<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Team Profile</title>
+    <link rel="stylesheet" href="profile.css">
+</head>
+<body>`)
+
+fs.writeFile('./public/index.html', htmlContent, (error) =>  err ? console.error(err) : console.log('Commit logged!'))
+
+//fs.writeFile('/index.html', htmlContent, (error) =>  err ? console.error(err) : console.log('Commit logged!'))
+
 const job = [];
 
 // const mainFunction = function(){
@@ -50,20 +64,19 @@ const team = [
     new Engineer("Sami", "sami1997@hotmail.com", "sami1997@github.com"),
     new Intern("Sarah", "saraht@gmail.com", "Aston University")
 ]
-console.log(team[1].role)
-// for(i=0; i<team.length; i++){
-//     if(team[i].jobRole === "e"){
-//         print("hello e!!!")
-//     }else if(team[i].jobRole === "m"){
-//         print("hello m!!!")
-//     }else if (team[i].jobRole === "i"){
-//         print("hello i!!!")
-//     } else return
-// }
-// const htmlContent = [
-//     {person1.returnInfo()},
 
-// ]
+for(i=0; i<team.length; i++){
+    if(team[i].role === "e"){
+        console.log("hello Engineer")
+        console.log(team[i])
+    }else if(team[i].role === "m"){
+        console.log("hello Manager!!!")
+        console.log(team[i])
+    }else if (team[i].role === "i"){
+        console.log("hello Intern!!!")
+        console.log(team[i])
+    } else return
+}
 
 
-//fs.writeFile('/index.html', htmlContent, (error) =>  err ? console.error(err) : console.log('Commit logged!'))
+

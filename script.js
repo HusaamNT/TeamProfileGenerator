95ba435753a357f016fc6b54bbbff75d443ee159
Husaam NT <husnaeem@gmail.com>
2022-12-06 14:07:02 +0000

    added ending html code to job

--- script.js
+++ script.js
@@ -104,17 +104,16 @@ for(i=0; i<team.length; i++){
         `)
     } else return
 }
+
+job.push(`</div>
+</main>
+</body>
+</html>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js" integrity="sha512-aVKKRRi/Q/YV+4mjoKBsE4x3H+BkegoM/em46NNlCqNTmUYADjBbeNefNxYV7giUp0VxICtqdrbqU7iVaeZNXA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>    
+`
+)
 console.log(job)
-
-// job.push(`</div>
-// </main>
-// </body>
-// </html>
-// <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-// <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js" integrity="sha512-aVKKRRi/Q/YV+4mjoKBsE4x3H+BkegoM/em46NNlCqNTmUYADjBbeNefNxYV7giUp0VxICtqdrbqU7iVaeZNXA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>    
-// `
-// )
-
 // fs.writeFile('./public/index.html', job, (error) =>  err ? console.error(err) : console.log('Commit logged!'))
 
 
